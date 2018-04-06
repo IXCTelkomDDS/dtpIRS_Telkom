c05e0fc4bed486eed9e0b93c52ddfdafbd74d366
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-04-06 15:08:21 +0700

    update dtp_irsTelkom 6-4-2018

--- koneksi_db.php
+++ koneksi_db.php
@@ -3,7 +3,7 @@
   //$username = 'id4115751_haerdiansyah90';
   //$password = 'Ardi2290';
   $username = 'root';
-  $password = 'qweasd123!!';
+  $password = "";
   $connect = mysqli_connect($host, $username, $password);
   // if($connect) echo 'Connected'; //true
   //  else 'Not Connected Yet'; //false
