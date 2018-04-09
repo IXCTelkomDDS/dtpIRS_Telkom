63dcf11e8257f5f86166bd68757f3cd0a86c3647
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-04-09 09:36:24 +0700

    update admin/log_user.php 9-4-2018

--- admin/log_user.php
+++ admin/log_user.php
@@ -13,12 +13,12 @@
 	if(isset($_REQUEST['keyword']) && $_REQUEST['keyword']<>""){
 		//if search found
 		$keyword = $_REQUEST['keyword'];
-		$reload = "log-admin.php?pagination=true&keyword=$keyword";
+		$reload = "log_user.php?pagination=true&keyword=$keyword";
 		$sql = "SELECT lu.TGL_RUH, lu.KD_RUH, lu.COM_RUH, lu.USERNAME, up.user_type FROM log_user_pic lu JOIN user_pic up ON (lu.USERNAME = up.username) WHERE lu.TGL_RUH LIKE '%$keyword%' || lu.KD_RUH LIKE '%$keyword%' || lu.COM_RUH LIKE '%$keyword%' || lu.USERNAME LIKE '%$keyword%' || up.user_type LIKE '%$keyword%' ORDER BY KD_RUH ASC";
 		$result = mysqli_query($connect, $sql);
 	} else {
 		//if search not found
-		$reload = "log-admin.php?pagination=true";
+		$reload = "log_user.php?pagination=true";
 		$sql = "SELECT lu.TGL_RUH, lu.KD_RUH, lu.COM_RUH, lu.USERNAME, up.user_type FROM log_user_pic lu JOIN user_pic up ON (lu.USERNAME = up.username) ORDER BY KD_RUH ASC";
 		$result = mysqli_query($connect, $sql);
 	}
@@ -36,7 +36,7 @@
 ?>
 
 
-	<title>Log Activity User P.I.C</title>
+	<title>Log Activity User IRS</title>
 
 		<!-- MAIN -->
 		<div class="main">
