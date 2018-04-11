3d15b056bbd78d165d5487cf0b1416b884cf23c3
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-04-11 10:21:09 +0700

    update act_update_picSUbmission.php 11-4-2018

--- act_update_picSubmission.php
+++ act_update_picSubmission.php
@@ -20,6 +20,7 @@
       $jenis_file     = $_POST['JENIS_FILE_UPLOAD'];
       $jenis_lab      = $_POST['JENIS_LAB_UPLOAD'];
       $deskripsi      = $_POST['DESKRIPSI_UPLOAD'];
+      $url            = $_POST['URL'];
 
       //if($file_size > 0) {
 
@@ -39,7 +40,7 @@
           $nama_file = $file_name;
             move_uploaded_file($file_tmp, 'uploads/'.$nama_file);
 
-            $sql = "UPDATE upload_dtp SET USERNAME = '$username', TGL_UPDATE = NOW(), JUDUL_UPLOAD = '$judul', JENIS_FILE_UPLOAD = '$jenis_file', JENIS_LAB_UPLOAD = '$jenis_lab', DESKRIPSI_UPLOAD = '$deskripsi', NAMA_FILE_UPLOAD = '$filebaru' WHERE ID_UPLOAD = '$id_upload'";
+            $sql = "UPDATE upload_dtp SET USERNAME = '$username', TGL_UPDATE = NOW(), JUDUL_UPLOAD = '$judul', JENIS_FILE_UPLOAD = '$jenis_file', JENIS_LAB_UPLOAD = '$jenis_lab', DESKRIPSI_UPLOAD = '$deskripsi', NAMA_FILE_UPLOAD = '$filebaru', URL = '$url' WHERE ID_UPLOAD = '$id_upload'";
             $query = mysqli_query($connect,$sql);     
 
             if($query) { ?>
@@ -56,7 +57,7 @@
     
     <?php } ?>
   <?php } else {
-    $sql = "UPDATE upload_dtp SET USERNAME = '$username', TGL_UPDATE = NOW(), JUDUL_UPLOAD = '$judul', JENIS_FILE_UPLOAD = '$jenis_file', JENIS_LAB_UPLOAD = '$jenis_lab', DESKRIPSI_UPLOAD = '$deskripsi' WHERE ID_UPLOAD = '$id_upload'";
+    $sql = "UPDATE upload_dtp SET USERNAME = '$username', TGL_UPDATE = NOW(), JUDUL_UPLOAD = '$judul', JENIS_FILE_UPLOAD = '$jenis_file', JENIS_LAB_UPLOAD = '$jenis_lab', DESKRIPSI_UPLOAD = '$deskripsi', URL = '$url' WHERE ID_UPLOAD = '$id_upload'";
     $query = mysqli_query($connect,$sql);     
 
             if($query) { ?>
