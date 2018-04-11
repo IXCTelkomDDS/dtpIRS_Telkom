bd8c829a672a62178a80357a363ce05e616ead43
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-04-11 08:57:01 +0700

    update data-picResult.php pada folder pic 11-4-2018

--- pic/data-picResult.php
+++ pic/data-picResult.php
@@ -84,12 +84,12 @@ session_start();
 				<div class="modal-body" style="height: 85px;">
 				    <!-- display data in pop up -->
 
-				    <?php if(($data['lab_pic'] == $data_pic['lab_pic'])) { ?>
+				    <?php //if(($data['lab_pic'] == $data_pic['lab_pic'])) { ?>
 
 				    <p>Name : <?php echo $data_pic['name_pic'];?></p>
 				    <p>Phone : <?php echo $data_pic['phone'];?> &amp; Email : <?php echo $data_pic['email'];?></p>	
 
-				    <?php } ?>
+				    <?php //} ?>
 
 		        </div>
 
