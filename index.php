41428435f108e3bba8a44fa10f2279eee1817133
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-04-30 10:45:40 +0700

    update file index.php 30-4-2018

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	include "koneksi_db.php";
 	include "header.php";
 
+	$reload = "index.php?pagination=true";
 	$sql = "SELECT * FROM news ORDER BY tgl_news ASC";
 	$result = mysqli_query($connect, $sql);
 
@@ -159,9 +160,9 @@
 								<div class="media-body text-left">
 									<h4 class="media-heading" style="margin-top: 30px; margin-left: 50px;">---- IRS News ----</h4>
 
-									<?php while(($count<$rpp) && ($i<$tcount)){
-									  mysqli_data_seek($result,$i);
-									  $data = mysqli_fetch_array($result);
+									<?php //while(($count<$rpp) && ($i<$tcount)){
+									  //mysqli_data_seek($result,$i);
+									  //$data = mysqli_fetch_array($result);
 
 									  //while($data = mysqli_fetch_array($result)) {
 
