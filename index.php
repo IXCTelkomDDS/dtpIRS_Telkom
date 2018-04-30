da969a331377090005b2510979647ed87d5f1278
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-04-30 11:22:58 +0700

    update file index.php 30-4-2018

--- index.php
+++ index.php
@@ -184,7 +184,7 @@
 							</div>				
 						</div>
 
-						<div style="margin-left: 40px;"><?php echo paginate_one($reload, $page, $tpages);?></div>
+						<div align="center" style="margin-left: 40px;"><?php echo paginate_one($reload, $page, $tpages);?></div>
 
                 	</div>
                 	<!-- End Search -->
