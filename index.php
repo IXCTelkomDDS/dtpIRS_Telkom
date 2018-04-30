e4d03afa952e04760debd90e96ddcc8903576278
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-04-30 10:55:26 +0700

    update file index.php 30-4-2018

--- index.php
+++ index.php
@@ -171,7 +171,7 @@
 
 									<p style="margin-left: 40px;"><?php echo ++$no_urut;?>. &nbsp; <?php echo $data['judul_news'];?>
 										<br>
-										Read more... <?php echo '<a target="_blank" class="link-4" style="font-weight: bold;" href = " '.$data['link'].'">'.$data['link'].'<a>';?>
+										Read more... <?php echo '<a target="_blank" class="link-4" style="font-weight: bold;" href = " '.$data['link'].'"><a>';?>
 									</p>
 
 									<?php 
